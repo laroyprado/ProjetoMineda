77cb2b2e65b55bebed67554a4b0b6d0732e037c4
laroy prado <laroyprado@gmail.com>
2024-02-28 20:57:04 -0300

    feat: Inicia o projeto

---
+++ src/main/java/com/ProjetoMineda/ProjetoMineda/ProjetoMinedaApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoMineda.ProjetoMineda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMinedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMinedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjetoMineda/ProjetoMineda/ProjetoMinedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetoMineda.ProjetoMineda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMinedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
