8430f064d943a615e4ee3df8b6aabb6695e7bcf3
laroyprado <laroyprado@gmail.com>
2024-03-06 22:08:07 -0300

    feat: cria repositório autorização

---
+++ src/main/java/com/ProjetoMineda/ProjetoMineda/repository/AutorizacaoRepository.java
@@ -0,0 +1,7 @@
+package com.ProjetoMineda.ProjetoMineda.repository;
+
+import com.ProjetoMineda.ProjetoMineda.entity.Autorizacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorizacaoRepository extends JpaRepository<Autorizacao,Long> {
+}
