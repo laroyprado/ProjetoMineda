60da4a1a1eda2a6370091772c39c8d4ec746a704
laroyprado <laroyprado@gmail.com>
2024-03-06 22:07:46 -0300

    fix: adiciona mapeamento ao usuário entity

--- src/main/java/com/ProjetoMineda/ProjetoMineda/entity/Usuario.java
+++ src/main/java/com/ProjetoMineda/ProjetoMineda/entity/Usuario.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -22,4 +24,14 @@ public class Usuario {
 
     @Column(name = "usr_senha")
     private String senha;
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<Anotacao> anotacoes;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "uau_usuario_autorizacao",
+            joinColumns = {@JoinColumn(name="usr_id")},
+            inverseJoinColumns = {@JoinColumn(name = "aut_id")})
+    private Set<Autorizacao> autorizacoes;
+
 }
