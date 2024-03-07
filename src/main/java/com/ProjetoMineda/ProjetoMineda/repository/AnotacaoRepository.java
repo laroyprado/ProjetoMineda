cdb63a39278d3a4e24f3c9c1bd9761567cf7ba80
laroyprado <laroyprado@gmail.com>
2024-03-06 22:07:58 -0300

    feat: cria repositório anotação

---
+++ src/main/java/com/ProjetoMineda/ProjetoMineda/repository/AnotacaoRepository.java
@@ -0,0 +1,7 @@
+package com.ProjetoMineda.ProjetoMineda.repository;
+
+import com.ProjetoMineda.ProjetoMineda.entity.Anotacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnotacaoRepository extends JpaRepository< Anotacao,Long> {
+}
